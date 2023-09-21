1a6ec312f89bb0d3513c38fcf5e0c9c0ea70166a
Flowolotl <isaac.bayudan@gmail.com>
2023-09-21 16:28:10 -0700

    forced to do this

--- nature.lua
+++ nature.lua
@@ -1,21 +1,179 @@
 local nature = {}
 
 nature.Adamant = {
-    HP = 1,
     Attack = 1.1,
     Defense = 1,
     SpAttack = 0.9,
     SpDefense = 1,
     Speed = 1
 }
-
-nature.Serious = {
-    HP = 1,
+nature.Bashful = {
     Attack = 1,
     Defense = 1,
     SpAttack = 1,
     SpDefense = 1,
     Speed = 1
+}
+nature.Bold = {
+    Attack = 0.9,
+    Defense = 1.1,
+    SpAttack = 1,
+    SpDefense = 1,
+    Speed = 1
+}
+nature.Brave = {
+    Attack = 1.1,
+    Defense = 1,
+    SpAttack = 1,
+    SpDefense = 1,
+    Speed = 0.9
+}
+nature.Calm = {
+    Attack = 0.9,
+    Defense = 1,
+    SpAttack = 1,
+    SpDefense = 1.1,
+    Speed = 1
+}
+nature.Careful = {
+    Attack = 1,
+    Defense = 1,
+    SpAttack = 0.9,
+    SpDefense = 1.1,
+    Speed = 1
+}
+nature.Docile = {
+    Attack = 1,
+    Defense = 1,
+    SpAttack = 1,
+    SpDefense = 1,
+    Speed = 1
+}
+nature.Gentle = {
+    Attack = 1,
+    Defense = 0.9,
+    SpAttack = 1,
+    SpDefense = 1.1,
+    Speed = 1
+}
+nature.Hardy = {
+    Attack = 1,
+    Defense = 1,
+    SpAttack = 1,
+    SpDefense = 1,
+    Speed = 1
+}
+nature.Hasty = {
+    Attack = 1,
+    Defense = 0.9,
+    SpAttack = 1,
+    SpDefense = 1,
+    Speed = 1.1
+}
+nature.Impish = {
+    Attack = 1,
+    Defense = 1.1,
+    SpAttack = 0.9,
+    SpDefense = 1,
+    Speed = 1
+}
+nature.Jolly = {
+    Attack = 1,
+    Defense = 1,
+    SpAttack = 0.9,
+    SpDefense = 1,
+    Speed = 1.1
+}
+nature.Lax = {
+    Attack = 1,
+    Defense = 1.1,
+    SpAttack = 1,
+    SpDefense = 0.9,
+    Speed = 1
+}
+nature.Lonely = {
+    Attack = 1.1,
+    Defense = 0.9,
+    SpAttack = 1,
+    SpDefense = 1,
+    Speed = 1
+}
+nature.Mild = {
+    Attack = 1,
+    Defense = 0.9,
+    SpAttack = 1.1,
+    SpDefense = 1,
+    Speed = 1
+}
+nature.Modest = {
+    Attack = 0.9,
+    Defense = 1,
+    SpAttack = 1.1,
+    SpDefense = 1,
+    Speed = 1
+}
+nature.Naive = {
+    Attack = 1,
+    Defense = 1,
+    SpAttack = 1,
+    SpDefense = 0.9,
+    Speed = 1.1
+}
+nature.Naughty = {
+    Attack = 1.1,
+    Defense = 1,
+    SpAttack = 1,
+    SpDefense = 0.9,
+    Speed = 1
+}
+nature.Quiet = {
+    Attack = 1,
+    Defense = 1,
+    SpAttack = 1.1,
+    SpDefense = 1,
+    Speed = 0.9
+}
+nature.Quirky = {
+    Attack = 1,
+    Defense = 1,
+    SpAttack = 1,
+    SpDefense = 1,
+    Speed = 1
+}
+nature.Rash = {
+    Attack = 1,
+    Defense = 1,
+    SpAttack = 1.1,
+    SpDefense = 0.9,
+    Speed = 1
+}
+nature.Relaxed = {
+    Attack = 1,
+    Defense = 1.1,
+    SpAttack = 1,
+    SpDefense = 1,
+    Speed = 0.9
+}
+nature.Sassy = {
+    Attack = 1,
+    Defense = 1,
+    SpAttack = 1,
+    SpDefense = 1.1,
+    Speed = 0.9
+}
+nature.Serious = {
+    Attack = 1,
+    Defense = 1,
+    SpAttack = 1,
+    SpDefense = 1,
+    Speed = 1
+}
+nature.Timid = {
+    Attack = 0.9,
+    Defense = 1,
+    SpAttack = 1,
+    SpDefense = 1,
+    Speed = 1.1
 }
 
 return nature
